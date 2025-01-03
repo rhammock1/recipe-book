d42a0fa1227696bbe87ab4dd269aa8897c30dff3
rhammock1 <robert.hammock@gotab.io>
2025-01-03 12:06:18 -0500

    mv ingredient/instruction endpoints to /recipe

--- www/app/routes/recipes.js
+++ www/app/routes/recipes.js
@@ -1,7 +1,5 @@
 import express from 'express';
 import { getRecipes, getRecipeByUuid, createRecipe, updateRecipe, deleteRecipe } from '../actions/recipes';
-import { getIngredientsByRecipeUuid } from '../actions/ingredients';
-import { getInstructionsByRecipeUuid } from '../actions/instructions';
 import recipesPage from "../../pages/recipes.marko";
 
 const router = express.Router();
@@ -16,31 +14,5 @@ router.route('/')
     const recipe = await createRecipe(req.body);
     res.json(recipe);
   })
-
-router.route('/:uuid')
-  .get(async (req, res) => {
-    const recipe = await getRecipeByUuid(req.params.uuid);
-    res.json({ recipe });
-  })
-  .put(async (req, res) => {
-    const recipe = await updateRecipe(req.params.uuid, req.body);
-    res.json(recipe);
-  })
-  .delete(async (req, res) => {
-    await deleteRecipe(req.params.uuid);
-    res.status(204).send();
-  });
-
-router.get('/:recipe_uuid/ingredients', async (req, res) => {
-  const {params: {recipe_uuid}} = req;
-  const ingredients = await getIngredientsByRecipeUuid(recipe_uuid);
-  res.json(ingredients);
-});
-
-router.get('/:recipe_uuid/instructions', async (req, res) => {
-  const {params: {recipe_uuid}} = req;
-  const instructions = await getInstructionsByRecipeUuid(recipe_uuid);
-  res.json(instructions);
-});
 
 export default router;
